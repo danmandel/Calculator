6f01bb83d3216606561dd090db4f6edb54a35631
danmandel <danmandel@users.noreply.github.com>
2015-02-21 01:46:08 -0500

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,17 @@
+int b;
+	int sum;
+
+	cout << "Enter a number: \n"; // takes info from computer and gives it to user
+	cin >> a; //takes info from user and gives it to computer
+
+	cout << "Enter another number: \n";
+	cin >> b;
+
+	sum = a + b;
+	cout << "The sum of those numbers is " << sum << endl;
+	system("pause");
+	return 0;
+}
+
+// << input stream object
+// >> stream extraction operator
