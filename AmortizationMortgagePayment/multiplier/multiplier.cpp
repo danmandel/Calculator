c53781d757b3ceccc7db7f5b7f6038d985dffc70
danmandel <danmandel@users.noreply.github.com>
2015-02-21 12:31:14 -0500

    Update and rename AmortizationMortgagePayment/multiplier to AmortizationMortgagePayment/multiplier/multiplier.cpp

---
+++ AmortizationMortgagePayment/multiplier/multiplier.cpp
@@ -0,0 +1,8 @@
+#include "multiplier.h"
+
+double multiplier(double firstNum, double secondNum)
+{
+	/*This function multiplies two numbers and returns the result*/
+	return firstNum*secondNum;
+}
+
