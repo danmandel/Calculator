490a46da0676209bcb495177ada7832419915877
danmandel <danmandel@users.noreply.github.com>
2015-02-21 23:18:23 -0500

    Update Source.cpp
    added:
    +interest payment 
    +current principal payment
    +current balance
    +current loan month
    +while loop that iterates through monthsOfLoan and increments currLoanMonth by 1 each time.

--- AmortizationMortgagePayment/Source.cpp
+++ AmortizationMortgagePayment/Source.cpp
@@ -34,6 +34,18 @@ int main()
 	cout << "Monthly Payment: " << setiosflags(ios::fixed) //keep decimal point in particuar precision
 		<< setprecision(2) << payment << endl;
 
+	double currInterestPayment = 0.0;
+	double currPrincipalPayment = 0.0;
+	double currBalance = 0.0;
+	long currLoanMonth = 1;
+	currBalance = principal;
+
+	while (currLoanMonth <= monthsOfLoan){
+		cout << "Current Month: " << currLoanMonth << endl;
+		currLoanMonth++;
+	}
+
+	cout << "Loan complete"<< endl;
 	system("pause");
 	return 0;
 }
