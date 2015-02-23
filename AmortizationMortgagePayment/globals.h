fba8c47e9e8747b5abcce3617e0037639259b09b
danmandel <danmandel@users.noreply.github.com>
2015-02-22 21:55:55 -0500

    Update and rename AmortizationMortgagePayment to AmortizationMortgagePayment/globals.h

---
+++ AmortizationMortgagePayment/globals.h
@@ -0,0 +1,13 @@
+const int gMonthsInYear = 12;
+const int gPercentDenominator = 100;
+
+struct gAmortizeMonth
+{
+	int year;
+	int yearMonth;
+	long loanMonth;
+	double payment;
+	double pureInterest;
+	double paidDownPrincipal;
+	double principalBalance;
+};
