5ba961e019999e9a684dd36e500cf607518b5826
danmandel <danmandel@users.noreply.github.com>
2015-03-02 18:34:07 -0500

    Update Source.cpp

--- AmortizationMortgagePayment/Source.cpp
+++ AmortizationMortgagePayment/Source.cpp
@@ -7,9 +7,16 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-
+	cout << "argc: " << argc << endl;
+	cout << "argv[0]: " << argv[0] << endl;
+	
+	for (int i = 0; i < argc; i++)
+	{
+		cout << "argv[" << i << "]: " << argv[i] << endl;
+	}
+	
 	double principal = 0.0;
 	cout << "Enter the principal amount: ";
 	cin >> principal;
